01109817b90f7139624e24aed73082d4a62574e7
TaylorSwinton <taylorswinton1@gmail.com>
2020-01-13 14:02:54 -0600

    Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// your code here
+const URL = 'https://api.github.com';
+
+function getRepositories() {
+    const name = document.getElementById('username').value;
+    const uri = URL + '/users/' + name + '/repos';
+    const req = new HMLHttpRequest();
+    req.open('GET', uri)
+    req.send();
+}
